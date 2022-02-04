be8be99c6e7628a6ed8d60fb4425de7a2bc3b3db
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2022-02-04 15:07:59 -0800

    fix build for osx-arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,6 @@ cd build
 
 cmake -E env CXXFLAGS="${CXXFLAGS} \
 cmake .. ${CMAKE_ARGS} \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-      -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DENABLE_UNIT_TESTS=OFF \
       -DENABLE_FUNC_TESTS=OFF
 
