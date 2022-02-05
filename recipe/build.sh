37a543b3cdf51eee2beb0a30ed8c72f838e0f878
Matthias Wittgen <wittgen@slac.stanford.edu>
2022-02-05 11:16:39 -0800

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 set -e
+set -x
 
 mkdir build
 cd build
-
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-    sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=\"${BUILD_PREFIX}/bin/grpc_cpp_plugin\";g" ../source/intercept/proto/CMakeLists.txt
-fi
 
 cmake -E env CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" \
 cmake .. ${CMAKE_ARGS} \
       -DENABLE_UNIT_TESTS=OFF \
       -DENABLE_FUNC_TESTS=OFF
 
-cmake --build . --target install
+cmake --build . --verbose --target install
