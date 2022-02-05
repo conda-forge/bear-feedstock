21601c1a8dbe9d971602e4e835c338b6e88306ff
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2022-02-05 13:31:54 -0800

    fix build for osx-arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,16 @@
 set -e
 set -x
 
-echo $(uname -a)
-echo $(which protoc)
-echo $(file $(which protoc))
-
 mkdir build
 cd build
-
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=\"${BUILD_PREFIX}/bin/grpc_cpp_plugin\";g" ../source/intercept/proto/CMakeLists.txt
+    sed -ie "s;\${_PROTOBUF_PROTOC};${BUILD_PREFIX}/bin/protoc;g"  ../source/intercept/proto/CMakeLists.txt
+fi
+ 
 cmake -E env CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" \
 cmake .. ${CMAKE_ARGS} \
       -DENABLE_UNIT_TESTS=OFF \
       -DENABLE_FUNC_TESTS=OFF
 
-cmake --build . --verbose --target install
+cmake --build . --target install
