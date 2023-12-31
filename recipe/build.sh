b119e6c08df85368d7ae5d7e660baaebb6aa6d6f
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2023-12-31 15:51:59 -0800

    Fix build deps

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,6 @@ set -x
 
 mkdir build
 cd build
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-    sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=\"${BUILD_PREFIX}/bin/grpc_cpp_plugin\";g" ../source/intercept/proto/CMakeLists.txt
-    sed -ie "s;\${_PROTOBUF_PROTOC};${BUILD_PREFIX}/bin/protoc;g"  ../source/intercept/proto/CMakeLists.txt
-fi
  
 cmake -E env CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" \
 cmake ${CMAKE_ARGS} \
